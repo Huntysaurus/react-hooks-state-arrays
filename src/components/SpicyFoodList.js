61d34b19e6975a5442771230c6547f88fb26bf12
Jacob <yawjacob@yahoo.com>
2022-01-01 14:12:33 -0600

    commit tree 16081873e423e66993a0f0a3c48c239de2e97364

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewSpicyFood } from "../data";
 
+const foodList = foods.map((food) => (
+  <li key={food.id}>
+    {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  </li>
+));
+
+return (
+  <div>
+    <button onClick={handleAddFood}>Add New Food</button>
+    <ul>{foodList}</ul>
+  </div>
+);
+
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
   return (
